9ef141b9664399955339a171359bee548c3a1516
Pyleks <aleksander.andersen@outlook.ie>
2020-06-11 20:56:01 +0100

    Fixed typo in free_delivery_delta

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,7 +9,7 @@ def bag_contents(request):
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
-        free_delvery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
 
     else:
         delivery = 0
@@ -22,7 +22,7 @@ def bag_contents(request):
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
-        'free_delivery_delta': free_delvery_delta,
+        'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total
 
