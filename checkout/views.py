50f5219dd3bad08220a312eaf0914db7cbfe485b
Pyleks <aleksander.andersen@outlook.ie>
2020-06-28 11:40:07 +0100

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_from,
+        'stripe_public_key': 'pk_test_51Gyez9DsrD0PTTMnk2E1wSWmcTUFwtticNhUamlui4NmPBpADZEtw27bjuXax695SrBbsKrXVD40aQYR1WPvEiEq000Y5057fP',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
