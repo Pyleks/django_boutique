07e862472eaacaedefea60b64b5611306750abe4
Pyleks <aleksander.andersen@outlook.ie>
2020-07-06 11:15:17 +0100

    removed and changed secret key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7#g%#6e-+e%*eb%d3ktvog_thwcz82%l8^@noi$))i(mb0ht*o'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pyleks-boutique-ado.herokuapp.com', 'localhost']
 
