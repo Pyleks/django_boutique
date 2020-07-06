2864d282f3e81e4153528723a2737e854d8070b0
Pyleks <aleksander.andersen@outlook.ie>
2020-07-06 10:56:57 +0100

    Deployment test

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'profiles',
 
     # Other
     'crispy_forms',
@@ -126,6 +128,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -167,5 +170,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
+# TODO Need to figure out the env variables
+# TODO Revisit webhook
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51Gyez9DsrD0PTTMnk2E1wSWmcTUFwtticNhUamlui4NmPBpADZEtw27bjuXax695SrBbsKrXVD40aQYR1WPvEiEq000Y5057fP')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51Gyez9DsrD0PTTMn8ARnqJvnq1YashEKNhNonQZb2AUGrv7w8grxP3cmmS7aYAopnGkzuBX2YYTEIbOOct6qImKz00REWaKgEd')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
---
+++ checkout/__init__.py
@@ -0,0 +1,1 @@
+default_add_config = 'checkout.apps.CheckoutConfig'
